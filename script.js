770dfbe156d67d2e5959e74a33083de0afbd5cf1
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-10-19 19:33:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("pre")[0].innerText=document.getElementsByTagName("pre")[0].innerText.replace('{"media_comments": [', '{"media_comments": [\n '); //first line
+document.getElementsByTagName("pre")[0].innerText=document.getElementsByTagName("pre")[0].innerText.replace(']}', '\n]}'); //last line
+
+document.getElementsByTagName("pre")[0].innerText=document.getElementsByTagName("pre")[0].innerText.replace(/],/g, '],\n'); //every comment on a new line
+var j=0;
+document.getElementsByTagName("pre")[0].innerHTML=document.getElementsByTagName("pre")[0].innerHTML.replace(/\["/g, '["<span style="color:red">'); //red time start
+document.getElementsByTagName("pre")[0].innerHTML=document.getElementsByTagName("pre")[0].innerHTML.replace(/", "/g, function (txtVar) { j++; if (j%2==1) return '</span>", "'; else return '", "'; }); //red @ start
+var i=0;
+document.getElementsByTagName("pre")[0].innerHTML=document.getElementsByTagName("pre")[0].innerHTML.replace(/", "/g, function (txtVar) { i++; if (i%2==0) return '", "<span style="color:red">'; else return '", "'; }); //red @ start
+document.getElementsByTagName("pre")[0].innerHTML=document.getElementsByTagName("pre")[0].innerHTML.replace(/"]/g, '</span>"]'); //red @ ending
